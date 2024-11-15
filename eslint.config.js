a13b2133d7364f9ad7e7db8fd865062eab07f539
Jeffrey Sanford <jeffreysanford@gmail.com>
2024-11-15 13:37:19 -0600

    Update ESLint configuration and package dependencies for improved linting rules and compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 const nx = require('@nx/eslint-plugin');
+const { FlatCompat } = require('@eslint/eslintrc');
+const compat = new FlatCompat();
 
 module.exports = [
   ...nx.configs['flat/base'],
@@ -23,11 +25,41 @@ module.exports = [
           ],
         },
       ],
+      'rxjs/no-create': 'error',
+      'rxjs/no-promise': 'error',
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          prefix: 'app',
+          style: 'kebab-case',
+          type: 'element',
+        },
+      ],
+      '@angular-eslint/no-standalone-components': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      '@angular-eslint/use-injectable-provided-in': 'error',
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
     },
   },
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
+    files: ['**/*.html'],
+    plugins: {
+      '@angular-eslint/template': require('@angular-eslint/eslint-plugin-template'),
+    },
     rules: {},
   },
+  ...compat.extends('plugin:@angular-eslint/recommended'),
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  ...compat.extends('plugin:@typescript-eslint/recommended-requiring-type-checking'),
+  ...compat.extends('plugin:rxjs/recommended'),
+  ...compat.extends('plugin:@nx/angular'),
 ];
